635347be5204274964a799de46a11313c260142d
Gianluccagit config --global user.name Gianluccagit config --global user.name Gianlucca <gdmbuzo@inf.ufpel.edu.br>
2020-10-14 11:00:51 -0300

    Adicionando busca, menu e ajustes  nos printfs

--- agenda-pBuffer.c
+++ agenda-pBuffer.c
@@ -8,12 +8,13 @@ typedef struct contato{
 }contato;
 
 typedef struct variaveis{
+    int opcao;
     int i;
-    int j;
+    int contador;
     int tam;
     int numContatos;
     int posRemove;
-    char nomeRemove[50];
+    char auxNome[50];
 } variaveis;
 
 void *pBuffer;
@@ -35,22 +36,39 @@ void insere(){
 }
 
 void imprime(){
-    printf("\nAgenda:");
+    printf("\nAgenda:\n");
     contatos = pBuffer + sizeof(variaveis);
     for(var->i = 0; var->i < var->numContatos; var->i++){
         printf("\nNome: %s",contatos->nome);
         printf("\nTelefone: %s",contatos->telefone);
         contatos++;
     }
+    printf("\n");
+}
+
+void buscarNome(){
+    contatos = pBuffer + sizeof(variaveis);
+    var->contador = -1;
+    printf("Digite o nome que deseja buscar: ");
+    scanf("%s", var->auxNome);
+    for(var->i = 0; var->i < var->numContatos; var->i++){
+        if(strcmp(var->auxNome,contatos->nome)== 0){
+            printf("\nNome: %s  Telefone: %s",contatos->nome,contatos->telefone);
+            var->contador++;
+        }
+        contatos++;
+    }
+    if(var->contador == -1)
+        printf("\nNome nao encontrado na lista");
 }
 
 void remover(){
     printf("\nDigite o nome que deseja remover: ");
-    scanf("%s",var->nomeRemove);
+    scanf("%s",var->auxNome);
     var->posRemove = -1;
     contatos = pBuffer + sizeof(variaveis);
     for (var->i = 0; var->i < var->numContatos; var->i++){
-        if(strcmp(contatos->nome, var->nomeRemove)== 0)
+        if(strcmp(contatos->nome, var->auxNome)== 0)
             var->posRemove = var->i;
         if(var->i+1 == var->numContatos)
             auxRemove = contatos;
@@ -64,12 +82,49 @@ void remover(){
         var->numContatos--;
         pBuffer = realloc(pBuffer, sizeof(variaveis) + sizeof(contato)* var->numContatos);
         var = pBuffer;
-        printf("Nome removido cm sucesso");
+        printf("Nome removido com sucesso");
     }else{
         printf("A agenda nao contem esse nome");
     } 
 }
 
+void redireciona(){
+
+    
+    scanf("%d",&var->opcao);
+    getchar();
+    switch (var->opcao)
+    {
+    case 1:
+        insere();
+        break;
+    case 2:
+        imprime();
+        break;
+    case 3:
+        remover();
+        break;
+    case 4:
+        buscarNome();
+        break;
+    case 5:
+        printf("\nBye!\n");
+        break;    
+    
+    default:
+        break;
+    }
+}
+
+void menu(){
+
+    printf("\nOpcoes:");
+    printf("\n1-Insere");
+    printf("\n2-Listar");
+    printf("\n3-Remover");
+    printf("\n4-Buscar");
+    printf("\n5-Sair\n");
+}
 
 
 int main (){ 
@@ -78,13 +133,11 @@ int main (){
     var = pBuffer;
     var->numContatos = 0;
     var->tam = sizeof(variaveis);
-    insere();
-    insere();
-    insere();
-    imprime();
-    remover();
-    imprime();
-
-
+    printf("====Agenda====");
+    while (var->opcao != 5){
+        menu();
+        redireciona();
+    }
+    free(pBuffer);
     return 0;
 }
